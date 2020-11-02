46c6c0efa7ca4a5a8238a9dbd0487c508c1c47d3
Jeroen van Langen <jvanlangen@live.nl>
2020-11-02 13:52:55 +0100

    Update Program.cs
    Added license header

--- src/xtsp/Program.cs
+++ src/xtsp/Program.cs
@@ -1,4 +1,8 @@
-﻿using Microsoft.Extensions.DependencyInjection;
+//  
+// Copyright (c) vanLangen.biz. All rights reserved.  
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.  
+//  
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
 using System;
