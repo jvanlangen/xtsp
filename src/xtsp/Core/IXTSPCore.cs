49f07802dfe072399fb95f7bc5c8d7ed53c7d6a2
Jeroen van Langen <jvanlangen@live.nl>
2020-11-02 13:53:43 +0100

    Update IXTSPCore.cs
    Added license header

--- src/xtsp/Core/IXTSPCore.cs
+++ src/xtsp/Core/IXTSPCore.cs
@@ -1,4 +1,8 @@
-﻿using System.Threading.Tasks;
+//  
+// Copyright (c) vanLangen.biz. All rights reserved.  
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.  
+//  
+using System.Threading.Tasks;
 
 namespace xtsp.Core
 {
