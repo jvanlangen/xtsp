69cbca21dbe3204a100b64542d5bcc1f4b8e8918
jvanlangen <jvanlangen@live.nl>
2020-11-02 02:25:21 +0100

    XTSPCore assembly created, this will be the startup application

---
+++ src/xtsp/Core/XTSPCore.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xtsp.Core
+{
+    public interface IXTSPCore
+    {
+        Task Run();
+    }
+
+    public class XTSPCore : IXTSPCore
+    {
+        private ILogger<IXTSPCore> _logger;
+
+        public XTSPCore(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<IXTSPCore>();
+        }
+
+        public async Task Run()
+        {
+            _logger.LogInformation("Run is called");
+
+            await Task.Delay(1000);
+            _logger.LogInformation("bla");
+        }
+    }
+}
---
+++ src/xtsp/Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+
+using System;
+using System.Threading.Tasks;
+using xtsp.Core;
+
+namespace xtsp
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (var loggerFactory = LoggerFactory.Create(builder => builder.AddConsole()))
+            {
+                var serviceProvider = new ServiceCollection()
+                    .AddLogging()
+                    .AddSingleton<IXTSPCore, XTSPCore>()
+                    .AddSingleton<ILoggerFactory>(sp => loggerFactory)
+                    .BuildServiceProvider();
+
+                var logger = loggerFactory.CreateLogger<Program>();
+
+                logger.LogInformation("Starting application");
+
+                //do the actual work here
+                var core = serviceProvider.GetService<IXTSPCore>();
+                await core.Run();
+
+                logger.LogInformation("All done!");
+            }
+        }
+    }
+}
